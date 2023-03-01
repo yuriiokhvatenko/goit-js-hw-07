28ce2b95ea22d761701778f2e2a9bc187e951768
Yurii Okhvatenko <fess517@gmail.com>
2023-03-01 23:10:15 +0200

    hw7-2nd part

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+// Creating Markup
+const markupForTaks = galleryItems.map(({ preview, original, description }) => {
+    return `<li class="gallery__item">
+  <a class="gallery__item" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`
+}).join('');
+
+
+
+// adding markup to the page
+
+galleryRef.innerHTML = markupForTaks;
+
+// Creating a modal
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+ });
+
